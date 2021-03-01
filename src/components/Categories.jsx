9462da47fbbf77f09711af3c0bc771c92c587374
Gavin <gavinbrrtt@gmail.com>
2021-03-01 10:30:13 -0800

    Added some new categories.

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,12 +3,29 @@ export const Categories = [
 	{"category": "Existentialism", "path": "existentialism"},
 	{"category": "Feminism", "path": "feminism"},
 	{"category": "German Idealism", "path": "gidealism"},
+	{"category": "Greek Philosophy", "path": "greek"},
 	{"category": "Logic", "path": "logic"},
 	{"category": "Marxism", "path": "marxism"},
 	{"category": "Metaphysics", "path": "metaphysics"},
+	{"category": "Modern Philosophy", "path": "modern"},
+	{"category": "Philosophy of Law", "path": "law"},
+	{"category": "Philosophy of Mathematics", "path": "math"},
 	{"category": "Philosophy of Mind", "path": "mind"},
 	{"category": "Philosophy of Religion", "path": "religion"},
 	{"category": "Philosophy of Science", "path": "science"},
 	{"category": "Political Philosophy", "path": "political"},
+	{"category": "Postmodern Philosophy", "path": "postmodernism"},
 	{"category": "Skepticism", "path": "skepticism"},
 ];
+
+/*
+
+Add:
+	-> Ethics
+	-> Epistemology
+	-> Philosophy of Language
+	-> Islamic Philosophy
+	-> Continental Philosophy
+	-> Logical Positivism
+
+*/
